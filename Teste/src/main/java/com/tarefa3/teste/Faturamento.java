7355c2ec03ea93eb41db50f6ecc0551018950101
IsabellaCorreiadosantos <cisabella626@gmail.com>
2024-09-04 22:24:23 -0300

    Tarefas

---
+++ Teste/src/main/java/com/tarefa3/teste/Faturamento.java
@@ -0,0 +1,23 @@
+package com.tarefa3.teste;
+
+public class Faturamento {
+    private int dia;
+    private double valor;
+
+    
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
